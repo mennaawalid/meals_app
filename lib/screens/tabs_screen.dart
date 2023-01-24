eae93c971d4bb369e5c6d673d7a2eb666196cc1e
menna walid <mennawalidrahma@gmail.com>
2023-01-24 02:25:14 +0200

    passed the fav list to the fav page

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,21 +1,37 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:flutter/material.dart';
 
+import '../models/meal.dart';
 import '../widgets/main_drawer.dart';
 import 'categories_screen.dart';
 import 'favorite_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-  const TabsScreen({super.key});
+  final List<Meal> favMeals;
+  const TabsScreen({
+    super.key,
+    required this.favMeals,
+  });
 
   @override
   State<TabsScreen> createState() => _TabsScreenState();
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Map<String, Object>> pages = [
-    {'page': const CategoriesScreen(), 'title': 'Categories'},
-    {'page': const FavoritesScreen(), 'title': 'Favorites'},
-  ];
+  late List<Map<String, Object>> pages;
+  @override
+  void initState() {
+    pages = [
+      {'page': const CategoriesScreen(), 'title': 'Categories'},
+      {
+        'page': FavoritesScreen(
+          favMeals: widget.favMeals,
+        ),
+        'title': 'Favorites'
+      },
+    ];
+    super.initState();
+  }
 
   int _selectedPageIndex = 0;
 
