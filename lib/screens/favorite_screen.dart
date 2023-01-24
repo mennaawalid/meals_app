4cff81112162e6c44781f763a3d05cce23b8520a
menna walid <mennawalidrahma@gmail.com>
2023-01-24 02:26:08 +0200

    created the favorites screen

--- lib/screens/favorite_screen.dart
+++ lib/screens/favorite_screen.dart
@@ -1,12 +1,31 @@
 import 'package:flutter/material.dart';
+import '../models/meal.dart';
+import '../widgets/meal_item.dart';
 
 class FavoritesScreen extends StatelessWidget {
-  const FavoritesScreen({super.key});
+  final List<Meal> favMeals;
+  const FavoritesScreen({required this.favMeals, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('favorites!'),
-    );
+    if (favMeals.isEmpty) {
+      return const Center(
+        child: Text('You currently have no favorites, start adding some!!'),
+      );
+    } else {
+      return ListView.builder(
+          itemBuilder: (ctx, i) {
+            return MealItem(
+              id: favMeals[i].id,
+              title: favMeals[i].title,
+              imageUrl: favMeals[i].imageUrl,
+              affordability: favMeals[i].affordability,
+              duration: favMeals[i].duration,
+              complexity: favMeals[i].complexity,
+            );
+          },
+        itemCount: favMeals.length,
+      );
+    }
   }
 }
